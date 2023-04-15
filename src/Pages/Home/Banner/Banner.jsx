6f0daafa3787611a04fdb57ed66554de8a9f7968
Saif Maamun <sadifmamun@gmail.com>
2023-04-15 15:14:03 +0600

    hero text done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,72 +1,65 @@
-import React from 'react';
-import img1 from '../../.././assets/images/banner/1.jpg'
-import img2 from '../../.././assets/images/banner/2.jpg'
-import img3 from '../../.././assets/images/banner/3.jpg'
-import img4 from '../../.././assets/images/banner/4.jpg'
-import img5 from '../../.././assets/images/banner/5.jpg'
-import img6 from '../../.././assets/images/banner/6.jpg'
+import React from "react";
+import img1 from "../../.././assets/images/banner/1.jpg";
+import img2 from "../../.././assets/images/banner/2.jpg";
+import img3 from "../../.././assets/images/banner/3.jpg";
+import img4 from "../../.././assets/images/banner/4.jpg";
+import img5 from "../../.././assets/images/banner/5.jpg";
+import img6 from "../../.././assets/images/banner/6.jpg";
+import './Banner.css'
+import CarouselItem from "./CarouselItem/CarouselItem";
 
 const Banner = () => {
-    return (
-        <div className="carousel  w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <img src={img1} className="w-full" />
-    <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-      <a href="#slide4" className="btn btn-circle mr-5">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
+
+    const bannerData = [
+        {
+          image: img1,
+          prev: 6,
+          id: 1,
+          next: 2,
+        },
+        {
+          image: img2,
+          prev: 1,
+          id: 2,
+          next: 3,
+        },
+        {
+          image: img3,
+          prev: 2,
+          id: 3,
+          next: 4,
+        },
+        {
+          image: img4,
+          prev: 3,
+          id: 4,
+          next: 5,
+        },
+        {
+          image: img5,
+          prev: 4,
+          id: 5,
+          next: 6,
+        },
+        {
+          image: img6,
+          prev: 5,
+          id: 6,
+          next: 1,
+        },
+      ];
+
+
+  return (
+    <div className="carousel  w-full">
+      {
+        bannerData.map(slide=><CarouselItem
+        key={slide.id}
+        slide={slide}
+        ></CarouselItem>)
+      }
     </div>
-    {/* hero title */}
-    <div className="absolute flex justify-start transform -translate-y-1/2 left-24 top-1/4 ">
-      <h1 className='font-bold text-7xl text-start text-white'>Affordable <br />Price For Car <br /> Servicing</h1>
-    </div>
-    {/* hero subtitle */}
-    <div className="w-1/2 absolute flex justify-start transform -translate-y-1/2 left-24 top-1/2 ">
-      <p className='text-xl text-start text-white'>There are many variations of passages of  available, but <br /> the majority have suffered alteration in some form</p>
-    </div>
-    {/* hero button */}
-    <div className="absolute flex justify-start transform -translate-y-1/2 left-24 top-2/3 ">
-    <button className="btn btn-active btn-secondary mr-5">Button</button>
-    <button className="btn btn-outline btn-secondary">Button</button>
-      
-    </div>
-  </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <img src={img2} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img src={img3} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide4" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide4" className="carousel-item relative w-full">
-    <img src={img4} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide5" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-  <div id="slide5" className="carousel-item relative w-full">
-    <img src={img5} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide4" className="btn btn-circle">❮</a> 
-      <a href="#slide6" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-  <div id="slide6" className="carousel-item relative w-full">
-    <img src={img6} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide5" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Banner;
---
+++ src/Pages/Home/Banner/CarouselItem/CarouselItem.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const CarouselItem = ({slide}) => {
+
+    const {image,prev,id,next} = slide;
+
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+            <div className='carousel-image'>
+        <img src={image} className="w-full" />
+
+            </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href={`#slide${prev}`} className="btn btn-circle mr-5">
+            ❮
+          </a>
+          <a href={`#slide${next}`} className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+        {/* hero title */}
+        <div className="absolute flex justify-start transform -translate-y-1/2 left-24 top-1/4 ">
+          <h1 className="font-bold text-7xl text-start text-white">
+            Affordable <br />
+            Price For Car <br /> Servicing
+          </h1>
+        </div>
+        {/* hero subtitle */}
+        <div className="w-1/2 absolute flex justify-start transform -translate-y-1/2 left-24 top-1/2 ">
+          <p className="text-xl text-start text-white">
+            There are many variations of passages of available, but <br /> the
+            majority have suffered alteration in some form
+          </p>
+        </div>
+        {/* hero button */}
+        <div className="absolute flex justify-start transform -translate-y-1/2 left-24 top-2/3 ">
+          <button className="btn btn-active btn-secondary mr-5">Button</button>
+          <button className="btn btn-outline btn-secondary">Button</button>
+        </div>
+      </div>
+    );
+};
+
+export default CarouselItem;
+
+
+
